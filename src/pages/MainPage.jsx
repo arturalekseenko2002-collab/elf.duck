0263cecf8001798955d4599a54decf6e149643f6
НовыйИмя <Email>
2026-01-23 15:09:07 +0100

    mainPage start

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -20,232 +20,68 @@ import championIMG from "../assets/championIMG.png";
 const MainPage = () => {
   const { user, userLoading, initials, displayName, displayUsername } = useUser();
 
-  const items = [
-    { id: 1, title: "UFC" },
-    { id: 2, title: "UFC" },
-    { id: 3, title: "UFC" },
-    { id: 4, title: "UFC" },
-  ];
-
   const navigate = useNavigate();
-
-  const openCase = (id) => {
-    navigate(`/case/${id}`);
-  };
 
   return (
     <div className="App">
+
       <div className="Main_Window">
+
         <div className="mainHomePageContainer">
+
           <div className="headerContainer">
-
-            <div className="CircleAndNickNameContainer">
-              <div className="circleInHeaderContainer">
-                {user?.photoUrl ? (
-                  <img src={user.photoUrl} className="userAvatar" />
-                ) : (
-                  <div className="circleName">
-                    {userLoading ? "" : initials}
-                  </div>
-                )}
-              </div>
-              <div className="nickNameContainer">
-                <div className="nickNameContainerPart1">
-                  {userLoading ? "Загрузка" : displayName}
-                </div>
-                <div className="nickNameContainerPart2">
-                  {userLoading ? "" : displayUsername}
-                </div>
-              </div>
-            </div>
-
-            <div className="deposit-ton-key-Container">
-              <div className="depositBalanceContainer">
-                <img src={deposit}/>
-              </div>
-              <div className="tonBalanceContainer">
-                <img src={ton}/>
-                <h2>0.00</h2> 
-              </div>
-              <div className="keyBalanceContainer">
-                <img src={key}/>
-                <h2>4</h2> 
-              </div>
-            </div>
-
+              
           </div>
 
-          <div className="titleAnimationContainer-cases">
-            
-            <h2>БЕСПЛАТНЫЕ КЕЙС</h2>
-          </div>
+        </div> 
+ 
 
-          <div className="casesGrid">
 
-            {items.map((item) => (
-              <div key={item.id} 
-                className="casesGridItem"    
-                  onClick={() => openCase(item.id)}
-                  role="button"
-                  tabIndex={0}
-                  onKeyDown={(e) => (e.key === "Enter" || e.key === " ") && openCase(item.id)}
-                >
-                  <div className="caseCard">
-                    <div className="caseCardImage">
-                      <img src={ufcCaseIMG} />
-                    </div>
-                    <div className="caseCardTitle">
-                      UFC
-                    </div>
-                    <div className="caseCardButton">
-                      открыть за подписку
-                    </div>
-                  </div>
+
+
+
+
+        {/* <div className="footerContainer">
+
+          <div className="footerInner">
+            <div className="footerNav">
+
+              <div className="footerItem">
+                <img src={lotteryIcon} />
+                <span>Лотереи</span>
               </div>
-            ))}
 
-            <div className="casesGridItem" onClick={() => openCase("g63")}>
-              <div className="caseCard">
-                <div className="caseCardImage">
-                  <img src={g63IMG} alt="" />
-
-                  <div className="caseProgressBadge">
-                    ⚡ 8 382 / 10 000
-                  </div>
-
-                  <div className="caseNewBadge">NEW</div>
-                </div>
-
-                <div className="caseCardTitle">G63</div>
-
-                <div className="caseCardActionRow">
-                  <div className="caseOpenPill">открыть за</div>
-
-                  <div className="casePrice">
-                    <span className="casePriceIconWrap ton">
-                      <img src={ton} alt="" />
-                    </span>
-                    <span className="casePriceValue">10</span>
-                  </div>
-                </div>
+              <div className="footerItem">
+                <img src={upgradeIcon} />
+                <span>Апгрейды</span>
               </div>
+
+              <div className="footerItem active">
+                <img src={casesIcon} alt="" />
+                <span>Кейсы</span>
+
+                <img
+                  className="footerActiveAnim"
+                  src="https://mycs2.pro/public/video/fire_orange.webp?v=3"
+                  alt=""
+                />
+              </div>
+
+              <div className="footerItem">
+                <img src={friendsIcon} />
+                <span>Друзья</span>
+              </div>
+
+              <div className="footerItem">
+                <img src={jetGiftsIcon} />
+                <span>Jet Gifts</span>
+              </div>
+
             </div>
-
           </div>
 
-          <div className="titleAnimationContainer-cases">
-            
-            <h2>ЛИМИТИРОВАННЫЕ КЕЙСЫ</h2>
-          </div>
+        </div> */}
 
-          <div className="casesGrid">
-
-            {items.map((item) => (
-              <div key={item.id} className="casesGridItem">
-                  <div className="caseCard">
-                    <div className="caseCardImage">
-                      <img src={pinkCaseIMG} />
-                    </div>
-                    <div className="caseCardTitle">
-                      PEPE
-                    </div>
-                    <div className="caseCardButton">
-                      открыть за подписку
-                    </div>
-                  </div>
-              </div>
-            ))}
-
-            {/* 2️⃣ Бесплатный кейс — за TON + progress */}
-            <div className="casesGridItem" onClick={() => openCase("UFC")}>
-              <div className="caseCard">
-                <div className="caseCardImage">
-                  <img src={ufcCaseIMG} alt="" />
-
-                  <div className="caseProgressBadge">
-                    ⚡ 782 / 1 500
-                  </div>
-                </div>
-
-                <div className="caseCardTitle">UFC</div>
-
-                <div className="caseCardActionRow">
-                  <div className="caseOpenPill">открыть за</div>
-
-                  <div className="casePrice">
-                    <span className="casePriceIconWrap ton">
-                      <img src={ton} alt="" />
-                    </span>
-                    <span className="casePriceValue">50</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            {/* 3️⃣ Бесплатный кйс — за ключи + NEW */}
-            <div className="casesGridItem" onClick={() => openCase("Чемпион")}>
-              <div className="caseCard">
-                <div className="caseCardImage">
-                  <img src={championIMG} alt="" />
-
-                  <div className="caseNewBadge">NEW</div>
-                </div>
-
-                <div className="caseCardTitle">Чемпион</div>
-
-                <div className="caseCardActionRow">
-                  <div className="caseOpenPill">открыть за</div>
-
-                  <div className="casePrice">
-                    <span className="casePriceIconWrap keys">
-                      <img src={key} alt="" />
-                    </span>
-                    <span className="casePriceValue">60</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-
-
-          <div className="footerContainer">
-
-            <div className="footerInner">
-              <div className="footerNav">
-                <div className="footerItem">
-                  <img src={lotteryIcon} />
-                  <span>Лотереи</span>
-                </div>
-                <div className="footerItem">
-                  <img src={upgradeIcon} />
-                  <span>Апгрейды</span>
-                </div>
-                <div className="footerItem active">
-                  <img src={casesIcon} alt="" />
-                  <span>Кейсы</span>
-
-                  <img
-                    className="footerActiveAnim"
-                    src="https://mycs2.pro/public/video/fire_orange.webp?v=3"
-                    alt=""
-                  />
-                </div>
-                <div className="footerItem">
-                  <img src={friendsIcon} />
-                  <span>Друзья</span>
-                </div>
-                <div className="footerItem">
-                  <img src={jetGiftsIcon} />
-                  <span>Jet Gifts</span>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
       </div>
     </div>
   );
