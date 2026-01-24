ca715ed333288f77e53ccf0d0d3c9b966dd35380
НовыйИмя <Email>
2026-01-24 16:23:24 +0100

    mainPage header

--- src/utils/haptics.js
+++ src/utils/haptics.js
@@ -1,11 +1,11 @@
-export const haptic = {
+export const tapHaptic = {
   light: () => window.Telegram?.WebApp?.HapticFeedback?.impactOccurred("light"),
   medium: () => window.Telegram?.WebApp?.HapticFeedback?.impactOccurred("medium"),
   heavy: () => window.Telegram?.WebApp?.HapticFeedback?.impactOccurred("heavy"),
   soft: () => window.Telegram?.WebApp?.HapticFeedback?.impactOccurred("soft"),
   rigid: () => window.Telegram?.WebApp?.HapticFeedback?.impactOccurred("rigid"),
 
-  success: () => window.Telegram?.WebApp?.HapticFeedback?.notificationOccurred("success"),
+  success: () =>  window.Telegram?.WebApp?.HapticFeedback?.notificationOccurred("success"),
 
   error: () => window.Telegram?.WebApp?.HapticFeedback?.notificationOccurred("error"),
 
